bc2d67ba1573d6d03e892efdb649354d1bd9110a
Troian Yevhenii <vitalich.ua@gmail.com>
2022-01-23 18:22:38 +0200

    complete second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 
+import Player from '@vimeo/player';
+ import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+const onPlay = function (data) {
+    
+    localStorage.setItem("videoplayer-current-time", data.seconds );
+};
+
+const currentTimeThrottle = player.on('timeupdate', throttle(onPlay, 1000) );
+const getCurrentTime = localStorage.getItem('videoplayer-current-time')
+
+
+player.setCurrentTime(getCurrentTime).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+           
+            break;
+    }
+});
+
